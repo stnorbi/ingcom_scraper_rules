ea838ba7a6acfdb1084a6c5b5f0c821ad4e7b73b
Stréling Norbert <streling.norbert@gmail.com>
2022-04-24 19:05:34 +0200

    Inital commit of CloudWatch Rules and lambda events

---
+++ scripts/validate_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+TEMPLATE='scrapyCronRules.json'
+NESTED_TEMPLATE='scrapyCronStack.json'
+STACK='scrapyCronRules'
+
+echo 'Validating templates...'
+aws cloudformation validate-template --template-body file://${TEMPLATE}
+aws cloudformation validate-template --template-body file://${NESTED_TEMPLATE}
+if [ $? -ne 0 ]; then
+    echo "Error validating templates for stack: ${STACK}"
+    exit $?
+fi
+echo 'Packaging templates...'
+aws cloudformation package --template-file ${TEMPLATE} --s3-bucket 'bwstacks' --s3-prefix 'scrapy-stacks' --output-template-file out/packaged-"${TEMPLATE}" --use-json
+if [ $? -ne 0 ]; then
+    echo "Error packaging stack: ${STACK}"
+    exit $?
+fi
+echo "Deploying revisions..."
+aws cloudformation deploy --template-file out/packaged-"${TEMPLATE}" --stack-name ${STACK}
+if [ $? -ne 0 ]; then
+    echo "Error deploying stack: ${STACK}"
+    exit $?
+fi
+echo "Deployment script exiting with exit code: $?"
