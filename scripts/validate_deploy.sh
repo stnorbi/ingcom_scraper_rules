ed876e597e59be14741818f41af9bad5dce321bf
Stréling Norbert <streling.norbert@gmail.com>
2022-11-02 21:50:20 +0100

    stack update

--- scripts/validate_deploy.sh
+++ scripts/validate_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Disabled scraper!"
 
 TEMPLATE='scrapyCronRules.json'
 NESTED_TEMPLATE='scrapyCronStack.json'
@@ -10,18 +11,18 @@ aws cloudformation validate-template --template-body file://${TEMPLATE}
 aws cloudformation validate-template --template-body file://${NESTED_TEMPLATE}
 if [ $? -ne 0 ]; then
     echo "Error validating templates for stack: ${STACK}"
-    exit $?
+    # exit $?
 fi
 echo 'Packaging templates...'
-aws cloudformation package --template-file ${TEMPLATE} --s3-bucket 'bwstacks' --s3-prefix 'scrapy-stacks' --output-template-file out/packaged-"${TEMPLATE}" --use-json
+aws cloudformation package --template-file ${TEMPLATE} --s3-bucket 'bwstacks' --s3-prefix 'scraper-stacks' --output-template-file out/packaged-"${TEMPLATE}" --use-json
 if [ $? -ne 0 ]; then
     echo "Error packaging stack: ${STACK}"
-    exit $?
+    # exit $?
 fi
 echo "Deploying revisions..."
 aws cloudformation deploy --template-file out/packaged-"${TEMPLATE}" --stack-name ${STACK}
 if [ $? -ne 0 ]; then
     echo "Error deploying stack: ${STACK}"
-    exit $?
+    # exit $?
 fi
 echo "Deployment script exiting with exit code: $?"
